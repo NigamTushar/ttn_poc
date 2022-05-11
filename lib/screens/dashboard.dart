8180325476a34dcff02dbaaf2a2134069e388671
Rahul <rahul.sharma1@tothenew.com>
2022-05-11 15:35:05 +0530

    application drawer UI design

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/app_drawer.dart';
 import '../widgets/user_detail_card.dart';
 
 class DashboardHome extends StatelessWidget {
@@ -11,45 +12,27 @@ class DashboardHome extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.white70,
       appBar: AppBar(
-        title: const Text('App bar'),
+        leading: Builder(builder: (BuildContext context) {
+          return IconButton(
+            onPressed: () {
+              Scaffold.of(context).openDrawer();
+            },
+            icon: const Icon(
+              Icons.menu,
+              color: Colors.green,
+            ),
+          );
+        }),
+        title: const Text(
+          'App bar',
+          style: TextStyle(color: Colors.blueGrey),
+        ),
         backgroundColor: Colors.white70,
         elevation: 0,
       ),
-      body: ListView(
-        children: [
-          const SizedBox(
-            height: 10,
-          ),
-          const UserDetailCard(),
-
-          const SizedBox(height: 10,),
-
-          // Stakeholders card
-
-          Container(
-            decoration: const BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.all(Radius.circular(15.0)),
-            ),
-            margin: const EdgeInsets.symmetric(horizontal: 20),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: const [
-                Padding(
-                  padding:  EdgeInsets.all(8.0),
-                  child:  Text(
-                    'My Stakeholders',
-                    style: TextStyle(fontSize: 20,),
-                  ),
-                ),
-                Divider(
-                  thickness: 1.0,
-                ),
-                ListTile()
-              ],
-            ),
-          )
-        ],
+      drawer: appDrawer(),
+      body: Container(
+        color: Colors.redAccent,
       ),
     );
   }
