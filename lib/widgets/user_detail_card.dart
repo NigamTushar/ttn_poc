17f30f9f8c7d2fb1634cd847af05ace0c83660f6
Rahul <rahul.sharma1@tothenew.com>
2022-05-18 15:32:57 +0530

    added - staggered_grid_view dependency user_detail  - built a layout builder to handle the internal gridview of the card breakpoint - Wrapper class to handle the breakpoints for columns in gridview

--- lib/widgets/user_detail_card.dart
+++ lib/widgets/user_detail_card.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../helper/breakpoint.dart';
+
 class UserDetailCard extends StatelessWidget {
   const UserDetailCard({
     Key? key,
@@ -7,32 +9,61 @@ class UserDetailCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ConstrainedBox(
-      constraints: const BoxConstraints(minWidth: 400, maxWidth: 1000),
-      child: Container(
-        // constraints: const BoxConstraints(
-        //   minWidth: 100,
-        //   maxWidth: 600,
-        // ),
+    return LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+      final Breakpoint breakpoint = Breakpoint.fromConstraints(
+        constraints,
+        type: BreakType.material,
+      );
+      // final double width = constraints.maxWidth.roundToDouble();
+      return Container(
         decoration: const BoxDecoration(
           color: Colors.white,
-          // borderRadius: BorderRadius.all(Radius.circular(15.0)),
         ),
         margin: const EdgeInsets.all(20),
-        // width: _screenSize.width - 20.0,
-        // height: _screenSize.height * 0.65,
         child: Column(
           children: [
+            const SizedBox(
+              height: 10.0,
+            ),
+
+            // Employee Experience
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  margin: const EdgeInsets.symmetric(horizontal: 8.0),
+                  child: const Text('0y 9m'),
+                  decoration: const BoxDecoration(
+                    color: Colors.grey,
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                  ),
+                )
+              ],
+            ),
+
+            GridView.count(
+              shrinkWrap: true,
+              padding: EdgeInsets.all(breakpoint.gutters),
+              crossAxisCount: Breakpoint.useColumns(breakpoint.columns, 3),
+              crossAxisSpacing: breakpoint.gutters,
+              mainAxisSpacing: breakpoint.gutters,
+              // childAspectRatio: (300 / 215),
+              children: <Widget>[userDetailContact(), userDetailDescription()],
+            ),
+
             // Contact and upper part of the user detail card
-            userDetailContact(),
+            // userDetailContact(),
 
             // user detail - description about the user
-            userDetailDescription(),
+            // userDetailDescription(),
 
-            //
-
+            // profile and org chart buttons
             Container(
-              padding: const EdgeInsets.only(bottom: 10.0, right: 10.0), // To hide the button focus area in case of web.
+              padding: const EdgeInsets.only(
+                  bottom: 10.0,
+                  right: 10.0), // To hide the button focus area in case of web.
               width: double.infinity,
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.end,
@@ -54,30 +85,21 @@ class UserDetailCard extends StatelessWidget {
             )
           ],
         ),
-      ),
-    );
+      );
+    });
   }
+}
 
-  Column userDetailContact() {
-    return Column(
+// MARK: - user detail contact UI - upper part
+Container userDetailContact() {
+  return Container(
+    height: 215,
+    width: 300,
+    child: Column(
       crossAxisAlignment: CrossAxisAlignment.center,
       children: [
         const SizedBox(
-          height: 10.0,
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            Container(
-              padding: const EdgeInsets.symmetric(horizontal: 8.0),
-              margin: const EdgeInsets.symmetric(horizontal: 8.0),
-              child: const Text('0y 9m'),
-              decoration: const BoxDecoration(
-                color: Colors.grey,
-                borderRadius: BorderRadius.all(Radius.circular(20)),
-              ),
-            )
-          ],
+          height: 5.0,
         ),
         const CircleAvatar(
           backgroundColor: Colors.black,
@@ -122,90 +144,73 @@ class UserDetailCard extends StatelessWidget {
           width: 150,
         ),
       ],
-    );
-  }
+    ),
+  );
+}
 
-  Container userDetailDescription() {
-    return Container(
-      padding: const EdgeInsets.all(8.0),
-      margin: const EdgeInsets.only(top: 10.0, left: 10.0),
-      width: double.infinity,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const Text(
-            'Rahul Sharma',
-            style: TextStyle(
-              fontSize: 30,
-            ),
+// MARK:- user detail description - lower part
+Container userDetailDescription() {
+  return Container(
+    constraints:const  BoxConstraints(minWidth: 300),
+    height: 215,
+    padding: const EdgeInsets.all(8.0),
+    margin: const EdgeInsets.only(top: 10.0, left: 10.0),
+    width: 300,
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: const [
+        Text(
+          'Rahul Sharma',
+          style: TextStyle(
+            fontSize: 30,
           ),
-          const Text(
-            'Software Engineer',
-            style: TextStyle(
-              fontSize: 18,
-            ),
+        ),
+        Text(
+          'Software Engineer',
+          style: TextStyle(
+            fontSize: 18,
           ),
-          const SizedBox(
-            height: 20,
-          ),
-          SizedBox(
-            width: double.infinity,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: const [
-                Icon(Icons.house_outlined),
-                SizedBox(
-                  width: 15,
-                ),
-                Text('To The New Private Limited')
-              ],
-            ),
-          ),
-          SizedBox(
-            width: double.infinity,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: const [
-                // Tooltip(
-                //   message: 'Legal Entity',
-                //   child: Icon(Icons.house_outlined),
-                // ),
-                Icon(Icons.business),
-                SizedBox(
-                  width: 15,
-                ),
-                Text('Digital Native Businesses (DNB)')
-              ],
-            ),
-          ),
-          SizedBox(
-            width: double.infinity,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: const [
-                Icon(Icons.business_center_outlined),
-                SizedBox(
-                  width: 15,
-                ),
-                Text('iOS')
-              ],
-            ),
-          ),
-          SizedBox(
-            width: double.infinity,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: const [
-                Icon(Icons.card_membership),
-                SizedBox(
-                  width: 15,
-                ),
-                Text('4659')
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
-  }
+        ),
+        SizedBox(
+          height: 15,
+        ),
+        ListTile(
+          visualDensity: VisualDensity(horizontal: 0,vertical: -4),
+          dense: true,
+          leading: Icon(Icons.house_outlined),
+          title: Text('To The New Private Limited', style: TextStyle(fontSize: 16.0),),
+          iconColor: Colors.black,
+          contentPadding: EdgeInsets.zero,// EdgeInsets.only(left: 0.0, top: 0.0, bottom: 0.0),
+          minLeadingWidth: 15,
+        ),
+        ListTile(
+          visualDensity: VisualDensity(horizontal: 0,vertical: -4),
+          dense: true,
+          leading: Icon(Icons.business),
+          title: Text('Digital Native Businesses(DNB)', style: TextStyle(fontSize: 16.0),),
+          iconColor: Colors.black,
+          contentPadding: EdgeInsets.only(left: 0.0, top: 0.0, bottom: 0.0),
+          minLeadingWidth: 15,
+        ),
+        ListTile(
+          visualDensity: VisualDensity(horizontal: 0,vertical: -4),
+          dense: true,
+          leading: Icon(Icons.business_center_outlined),
+          title: Text('iOS', style: TextStyle(fontSize: 16.0),),
+          iconColor: Colors.black,
+          contentPadding: EdgeInsets.only(left: 0.0),
+          minLeadingWidth: 15,
+        ),
+        ListTile(
+          visualDensity: VisualDensity(horizontal: 0,vertical: -4),
+          dense: true,
+          leading: Icon(Icons.card_membership),
+          title: Text('4659', style: TextStyle(fontSize: 16.0),),
+          iconColor: Colors.black,
+          contentPadding: EdgeInsets.only(left: 0.0),
+          minLeadingWidth: 15,
+        ),
+      ],
+    ),
+  );
 }
