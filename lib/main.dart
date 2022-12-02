dbd9a0986a8d455283b48a15d2f1f34d4759a3b5
tushar <tushar.nigam@tothenew.com>
2022-12-02 13:35:19 +0530

    fixed overlapping on dashboard

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:newers_world/screens/dashboard.dart';
 
 void main() {
@@ -17,23 +17,27 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: 500,
-      child: MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blueGrey,
-          textTheme: const TextTheme(
-            bodyText1: TextStyle(),
-            bodyText2: TextStyle(),
-          ).apply(
-            bodyColor: Colors.blueGrey,
-            displayColor: Colors.blueGrey,
-          ),
-        ),
-        home: const DashboardHome(),
-        debugShowCheckedModeBanner: false,
-      ),
-    );
+    return ScreenUtilInit(
+        designSize: ScreenUtil.defaultSize,
+        builder: (BuildContext context, Widget? child) {
+          return SizedBox(
+            width: 500,
+            child: MaterialApp(
+              title: 'Flutter Demo',
+              theme: ThemeData(
+                primarySwatch: Colors.blueGrey,
+                textTheme: const TextTheme(
+                  bodyText1: TextStyle(),
+                  bodyText2: TextStyle(),
+                ).apply(
+                  bodyColor: Colors.blueGrey,
+                  displayColor: Colors.blueGrey,
+                ),
+              ),
+              home: const DashboardHome(),
+              debugShowCheckedModeBanner: false,
+            ),
+          );
+        });
   }
 }
