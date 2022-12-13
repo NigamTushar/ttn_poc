00cb128b0b8c6a48b9af91a1326d5a5b76b52015
lokeshkumarprajapati <lokesh.prajapati@tothenew.com>
2022-12-13 10:13:59 +0530

    rabase issue resolved

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:newers_world/dashboard_screens/dashboard_home_screen.dart';
+/*import 'package:newers_world/dashboard_screens/dashboard_home_screen.dart';
 import 'package:newers_world/widgets/app_drawer.dart';
 import 'package:newers_world/widgets/page_header.dart';
 import 'package:newers_world/widgets/profile_expandable_list.dart';
 import 'package:newers_world/widgets/profile_list.dart';
-import 'package:newers_world/widgets/stakeholder_list.dart';
+import 'package:newers_world/widgets/stakeholder_list.dart';*/
+
+import '../dashboard/widgets/app_drawer.dart';
+import '../dashboard/widgets/page_header.dart';
+import '../dashboard/widgets/profile_expandable_list.dart';
+import '../dashboard/widgets/profile_list.dart';
 final empSnapShots = {'Legal Entity': 'TO THE NEW Private Limited','Business Unit':'Australia & New Zealand (ANZ)','Function':'Delivery','Competency':'Android','Designation':'Senior Software Engineer'};
 final List<Map<String, String>> listOfColumns = [
   {
@@ -50,6 +55,35 @@ class _ProfileState extends State<Profile> {
   @override
   Widget build(BuildContext context) {
     var _screenSize = MediaQuery.of(context).size;
+    return SingleChildScrollView(
+      controller: ScrollController(),
+      child: Wrap(
+        children:  [
+          Container(
+            width: _screenSize.width/2.4,
+            color: Colors.white,
+            margin: const EdgeInsets.only(top: 20,left: 20),
+            padding: const EdgeInsets.all(20.0),
+            child: ExpandableListView(title: "Employement Snapshot", profileInfoMap: empSnapShots,),
+          ),
+          Container(
+            width: _screenSize.width/2.4,
+            color: Colors.white,
+            margin: const EdgeInsets.only(top: 20,left: 20),
+            padding: const EdgeInsets.all(20.0),
+            child: ExpandableListView(title: "Employement Snapshot", profileInfoMap: empSnapShots,),
+          ),
+
+          Container(
+            width: _screenSize.width,
+            color: Colors.white,
+            margin: const EdgeInsets.only(top: 20,left: 20),
+            padding: const EdgeInsets.all(20.0),
+            child: ProfileListView(title: "Profile", profileInfoMap: listOfColumns,heading: ['d'],),
+          )
+        ],
+      ),
+    );
     return LayoutBuilder(
         builder: (BuildContext context, BoxConstraints constraints) {
           return Row(
