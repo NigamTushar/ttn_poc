e3e9fabf8299df3fc8279b43877a7122ff97147b
tushar <tushar.nigam@tothenew.com>
2023-01-03 10:22:08 +0530

    TAB UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:newers_world/drawer/screen/drawer_screen.dart';
+import 'package:newers_world/tab_ui/routes/tab_router.dart';
 import 'package:seo_service/html/seo_controller.dart';
 import 'package:seo_service/html/tree/widget_tree.dart';
-
 import 'main_router.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(MyApp());
+  runApp(TabApp());
 }
 
 class App extends StatelessWidget {
@@ -40,6 +41,45 @@ class App extends StatelessWidget {
 class MyApp extends StatelessWidget {
   MyApp({Key? key}) : super(key: key);
   final _router = MainRouter();
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return SeoController(
+      enabled: true,
+      tree: WidgetTree(context: context),
+      child: ScreenUtilInit(
+          designSize: ScreenUtil.defaultSize,
+          builder: (BuildContext context, Widget? child) {
+            return SizedBox(
+              width: 500,
+              child: MaterialApp.router(
+                title: 'Flutter Demo',
+                theme: ThemeData(
+                  dividerColor: Colors.transparent,
+                  primarySwatch: Colors.blueGrey,
+                  textTheme: const TextTheme(
+                    bodyText1: TextStyle(),
+                    bodyText2: TextStyle(),
+                  ).apply(
+                    bodyColor: Colors.blueGrey,
+                    displayColor: Colors.blueGrey,
+                  ),
+                ),
+                // home: const DrawerScreen(),
+                debugShowCheckedModeBanner: false,
+                routerDelegate: _router.delegate(),
+                routeInformationParser: _router.defaultRouteParser(),
+              ),
+            );
+          }),
+    );
+  }
+}
+
+class TabApp extends StatelessWidget {
+  TabApp({Key? key}) : super(key: key);
+  final _router = TabRouter();
 
   // This widget is the root of your application.
   @override
